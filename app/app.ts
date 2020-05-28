ba1ffcbea78887c8f5f285e83a9b85a715601504
Jose D <joserafael161@hotmail.com>
2020-05-28 09:27:43 -0500

    Update dependencies

--- app/app.ts
+++ app/app.ts
@@ -1,1 +1,3 @@
-console.log('its working 2');
+import torrentStream from "torrent-stream";
+
+const pulpMagnet = "magnet:?xt=urn:btih:3F8F219568B8B229581DDDD7BC5A5E889E906A9B&dn=Pulp+Fiction+%281994%29+%5B1080p%5D+%5BYTS.LT%5D&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fp4p.arenabg.ch%3A1337&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337";
